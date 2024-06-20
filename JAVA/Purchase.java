111da348474a33d8a31578bc8dba9360cb0d7144
MP <99058277+analyticalthinker@users.noreply.github.com>
2024-06-20 15:37:05 -0600

    Add files via upload

--- JAVA/Purchase.java
+++ JAVA/Purchase.java
@@ -1,14 +1,14 @@
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
-package createpurchase;
+package purchase;
 
 /**
  *
- * @author RR
+ * @author u6023321
  */
-class Purchase {
+public class Purchase {
     private int invoiceNumber;
     private double saleAmount;
     private double salesTax;
